edd77a468b0728f5588eabdf2b0095ee3223f40c
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-09-06 09:28:55 +0700

    Update robocon_xbot.py

--- robocon_xbot.py
+++ robocon_xbot.py
@@ -1,14 +1,25 @@
-import time, gc
+import machine
 
+# support libs
 from setting import *
 from utility import *
 
+# hardware libs
+from pins import *
+from led import *
+from speaker import speaker, BIRTHDAY, TWINKLE, JINGLE_BELLS, WHEELS_ON_BUS, FUR_ELISE, CHASE, JUMP_UP, JUMP_DOWN, POWER_UP, POWER_DOWN
+from ultrasonic import ultrasonic
+from line_array import line_array
+from button import *
 from motor import motor
 from servo import servo
-from speaker import *
-from ultrasonic import *
-from line_array import line_array
+from led_matrix import Image, led_matrix
 from motion import motion
+from robot import robot
+
+# wireless libs
+from ble import ble_o, ble
+#from wifi import *
 def follow_line_until(speed, condition, port, timeout=10000):
 
 
